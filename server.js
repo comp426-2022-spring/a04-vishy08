7271d09fbcb86d5216efaec701165a97176ac623
vishy08 <vishyk@live.unc.edu>
2022-05-02 05:53:15 -0400

    second

--- server.js
+++ server.js
@@ -1,24 +1,88 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 
-const database = require("better-sqlite3")
+const database = require('better-sqlite3')
 
-var md5 = require("md5")
+var md5 = require('md')
 
-const logdb = require("./database.js")
+const logdb = require('./database.js')
 
 const myArgs = require('minimist')(process.argv.slice(2))
+myArgs['port', 'debug', 'log', 'help']
+var HTTP_PORT = myArgs.port || 5000 || myArgs.p
+
+// Start an app server
+const server = app.listen(HTTP_PORT, () => {
+  console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
+});
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-myArgs['port']
-var HTTP_PORT = myArgs.port || 5000
+// console.log(args)
+// Store help text 
+const help = (`
+server.js [options]
 
-// Start an app server
-const server = app.listen(HTTP_PORT, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
-});
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
+
+app.use((req, res, next) => {
+    let logdata = {
+    remoteaddr: req.ip,
+    remoteuser: req.user,
+    time: Date.now(),
+    method: req.method,
+    url: req.url,
+    protocol: req.protocol,
+    httpversion: req.httpVersion,
+    status: res.statusCode,
+    referer: req.headers['referer'],
+    useragent: req.headers['user-agent']
+  }
+  console.log(logdata)
+    const stmt = logdb.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+    next();
+})
+
+// If --help or -h, echo help text to STDOUT and exit
+if (myArgs.help || myArgs.h) {
+    console.log(help)
+    process.exit(0)
+}
+
+const ifDebug = myArgs.debug || false || myArgs.d
+if (myArgs.ifDebug) {
+  app.get('/app/log/access/', (req, res, next) => {
+    res.status(200).json(logdb.prepare('SELECT * FROM accesslog').all())
+  })
+  app.get('./app/error', (req, res, next) => {
+    throw new Error("Error test works.");
+  })
+  }
+
+  const ifLog = myArgs.log || myArgs.l || true
+  if (myArgs.ifLog == 'false') {
+    throw new Error("access file not created")
+  } else {
+    // Use morgan for logging to files
+    // Create a write stream to append (flags: 'a') to a file
+    const createAccessLog = fs.createWriteStream('access.log', { flags: 'a' })
+    // Set up the access logging middleware
+    app.use(morgan('combined', { stream: createAccessLog }))
+  }
 
 
 /** Simple coin flip
