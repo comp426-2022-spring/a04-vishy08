07194ce3812db6f0d39a88abee95abd5ee582cc7
vishy08 <vishyk@live.unc.edu>
2022-05-02 07:38:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ const server = app.listen(port, () => {
 
   app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
-    res.type("text/html")
+    //res.type("text/html")
   });
   
   app.get('/app/flip/', (req, res) => {
