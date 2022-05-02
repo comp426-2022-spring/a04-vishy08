4094a29601f881d55cdc33c96a723a64f155ab20
vishy08 <vishyk@live.unc.edu>
2022-05-02 08:57:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const logdb = require('./database.js')
 
 const args = require('minimist')(process.argv.slice(2))
 args['port', 'debug', 'log', 'help']
-var port = args.port || process.env.PORT || 5555
+var port = args.port || args.p || process.env.PORT || 5000
 
 // console.log(args)
 // Store help text 
